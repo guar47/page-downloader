4e99cc57540cccce497db656a2fe3f154e13536d
guar47 <guar47@ya.ru>
2017-02-14 13:47:54 +0300

    change regexp in filenames

--- __tests__/pageDownloader.test.js
+++ __tests__/pageDownloader.test.js
@@ -7,15 +7,27 @@ import httpAdapter from 'axios/lib/adapters/http';
 import pageLoader from '../src/';
 
 axios.defaults.adapter = httpAdapter;
+const htmlbody = fs.readFileSync('__tests__/__fixtures__/hexlet-io-courses.html', 'utf8');
 
+// test('check dowloaded file', () => {
+//   nock('http://localhost')
+//     .get('/testpath')
+//     .reply(200, 'test content http body');
+//   const address = 'http://localhost/testpath';
+//   const tmpDir = fs.mkdtempSync('__tests__/__fixtures__/tmp-');
+//   pageLoader(address, tmpDir).then(() => {
+//     const file = fs.readFileSync(`${tmpDir}/localhost-testpath.html`, 'utf8');
+//     expect(file).toBe('test content http body');
+//   });
+// });
 test('check dowloaded file', () => {
   nock('http://localhost')
     .get('/testpath')
-    .reply(200, 'test content http body');
+    .reply(200, htmlbody);
   const address = 'http://localhost/testpath';
   const tmpDir = fs.mkdtempSync('__tests__/__fixtures__/tmp-');
   pageLoader(address, tmpDir).then(() => {
     const file = fs.readFileSync(`${tmpDir}/localhost-testpath.html`, 'utf8');
-    expect(file).toBe('test content http body');
+    expect(file).toBe(htmlbody);
   });
 });
--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@
 import url from 'url';
 import fs from 'fs';
 import axios from 'axios';
+import cheerio from 'cheerio';
 
 export default (address, outputDir = '.') => {
   const parsedURL = url.parse(address);
-  const newFileName = `${parsedURL.hostname.replace(/[^0-9a-z]/gi, '-')}${parsedURL.pathname.replace(/\//g, '-')}.html`;
+  const newFileName = `${parsedURL.hostname.replace(/[^0-9a-z]/gi, '-')}${parsedURL.pathname.replace(/[^0-9a-z]/gi, '-')}.html`;
   return axios.get(address).then((response) => {
     fs.openSync(`${outputDir}/${newFileName}`, 'w');
     fs.writeFileSync(`${outputDir}/${newFileName}`, response.data);
