f17000819e0921a4c7bfa97d78c91ddc8c367d0b
guar47 <guar47@ya.ru>
2017-02-15 15:20:34 +0300

    added support download sub-links

--- __tests__/pageDownloader.test.js
+++ __tests__/pageDownloader.test.js
@@ -8,7 +8,20 @@ import pageLoader from '../src/';
 
 const htmlbody = fs.readFileSync('__tests__/__fixtures__/template/hexlet-io-courses.html', 'utf8');
 
-test('check dowloaded file', (done) => {
+test('main html download checker', (done) => {
+  nock('http://localhost')
+    .get('/testpath')
+    .reply(200, htmlbody);
+  const address = 'http://localhost/testpath';
+  const tmpDir = fs.mkdtempSync(os.tmpdir());
+  console.log(`Current tmp directory for tests - ${tmpDir}`);
+  pageLoader(address, tmpDir).then(() => {
+    const file = fs.readFileSync(`${tmpDir}/localhost-testpath.html`, 'utf8');
+    expect(file).toBe(htmlbody);
+    done();
+  });
+});
+test('main html download checker', (done) => {
   nock('http://localhost')
     .get('/testpath')
     .reply(200, htmlbody);
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import url from 'url';
 import fs from 'fs';
 import axios from 'axios';
 import path from 'path';
-// import cheerio from 'cheerio';
+import cheerio from 'cheerio';
 
 const nameGenerator = (address, type) => {
   const parsedURL = url.parse(address);
@@ -15,20 +15,43 @@ const nameGenerator = (address, type) => {
     case 'folder':
       newName = `${parsedURL.hostname.replace(/[^0-9a-z]/gi, '-')}${parsedURL.pathname.replace(/\//g, '-')}_files`;
       break;
+    case 'other':
+      newName = `${parsedURL.hostname.replace(/[^0-9a-z]/gi, '-')}${parsedURL.pathname.replace(/\//g, '-')}`;
+      break;
     default:
       return newName;
   }
   return newName;
 };
 
-export default (address, outputDir = '.') => {
+const linksLoader = (html, newFolderPath) => {
+  const $ = cheerio.load(html);
+  const links = [];
+  $('link').each((i, elem) => {
+    links[i] = $(elem).attr('href');
+  });
+  $('script[src]').each((i, elem) => {
+    links[i] = $(elem).attr('src');
+  });
+  links.join(', ');
+  if (links.length > 0) {
+    fs.mkdirSync(newFolderPath);
+    links.forEach((link) => {
+      const newFileName = nameGenerator(link, 'other');
+      return axios.get(link).then((response) => {
+        fs.writeFileSync(path.join(newFolderPath, newFileName), response.data, 'binary');
+      });
+    });
+  }
+};
+
+const pageLoader = (address, outputDir = '.') => {
   const newFileName = nameGenerator(address, 'html');
-  const newFolderName = nameGenerator(address, 'folder');
-  return axios.get(address, {
-    responseType: 'arraybuffer',
-  }).then((response) => {
+  const newFolderPath = path.join(outputDir, nameGenerator(address, 'folder'));
+  return axios.get(address).then((response) => {
     fs.writeFileSync(path.join(outputDir, newFileName), response.data, 'binary');
-  }).then(() => {
-    fs.mkdirSync(path.join(outputDir, newFolderName));
+    linksLoader(response.data, newFolderPath);
   });
 };
+
+export default pageLoader;
