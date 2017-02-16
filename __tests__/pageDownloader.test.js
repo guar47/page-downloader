15f32ed1cb4e84bfe57497806c25b6975f16f4ae
guar47 <guar47@ya.ru>
2017-02-16 19:24:40 +0300

    refactoring test

--- __tests__/pageDownloader.test.js
+++ __tests__/pageDownloader.test.js
@@ -9,7 +9,7 @@ import pageLoader from '../src/';
 const htmlbody = fs.readFileSync(path.join('__tests__', '__fixtures__', 'hexlet-io-courses.html'), 'utf8');
 const htmlbodyAfterSubst = fs.readFileSync(path.join('__tests__', '__fixtures__', 'hexlet-io-courses_subst.html'), 'utf8');
 const address = 'http://localhost/testpath';
-const tmpDir = os.tmpdir();
+const tmpDir = fs.mkdtempSync(os.tmpdir());
 
 beforeEach(() => {
   nock('http://localhost')
@@ -17,17 +17,14 @@ beforeEach(() => {
     .reply(200, htmlbody);
 });
 
-test('main html download checker and substitution links', (done) => {
+test('main html download checker', (done) => {
   pageLoader(address, tmpDir).then(() => {
     const mainFile = path.join(tmpDir, 'localhost-testpath.html');
+    const files = fs.readdirSync(path.join(tmpDir, 'localhost-testpath_files'));
+    const subFile = path.join(tmpDir, 'localhost-testpath_files', 'en-hexlet-io-lessons.rss');
     expect(fs.readFileSync(mainFile, 'utf8')).toBe(htmlbodyAfterSubst);
+    expect(fs.existsSync(subFile)).toBeTruthy();
+    expect(files.includes('en-hexlet-io-lessons.rss')).toBeTruthy();
     done();
   });
-});
-test('check subfiles download', (done) => {
-  const files = fs.readdirSync(path.join(tmpDir, 'localhost-testpath_files'));
-  const subFile = path.join(tmpDir, 'localhost-testpath_files', 'en-hexlet-io-lessons.rss');
-  expect(fs.existsSync(subFile)).toBeTruthy();
-  expect(files.includes('en-hexlet-io-lessons.rss')).toBeTruthy();
-  done();
 });
